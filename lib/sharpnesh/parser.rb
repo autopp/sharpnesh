3fad613f2713f4b24a2e51d6ee3a83bf765888d8
autopp <autopp.inc@gmail.com>
2018-05-02 23:53:36 +0900

    test: decl Parser#parse

--- lib/sharpnesh/parser.rb
+++ lib/sharpnesh/parser.rb
@@ -2,5 +2,7 @@ module Sharpnesh
   # Parser provides features of parsing a bash script
   #
   class Parser
+    def parse(io, name)
+    end
   end
 end
--- spec/sharpnesh/parser_spec.rb
+++ spec/sharpnesh/parser_spec.rb
@@ -4,6 +4,6 @@ describe Sharpnesh::Parser do
   let(:parser) { described_class.new }
 
   describe '#parse' do
-    subject { parser.parse(io, name) }
+    subject { parser.parse(StringIO.new(src), 'input.sh') }
   end
 end
