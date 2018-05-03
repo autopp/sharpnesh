571b71d8b8c5c2e32215e9e66c3667e747beb800
autopp <autopp.inc@gmail.com>
2018-05-03 22:15:06 +0900

    test: add test case for simple word

--- spec/sharpnesh/parser_spec.rb
+++ spec/sharpnesh/parser_spec.rb
@@ -8,6 +8,16 @@ describe Sharpnesh::Parser do
 
     context 'with simple word' do
       let(:src) { 'foo' }
+      let(:expected) do
+        n(:list,
+          body: n(:pipeline,
+                  time: nil, opt_p: nil, excl: nil,
+                  command: n(:simple_cmd,
+                             assigns: [], body: [n(:str, body: 'foo')])),
+          terminal: 'EOS', next: nil)
+      end
+
+      it { is_expected.to eq(expected) }
     end
   end
 end
