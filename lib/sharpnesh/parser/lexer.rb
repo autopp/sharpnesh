b976fe0633a24c759885262957c222b33deebd2f
autopp <autopp.inc@gmail.com>
2018-05-05 23:47:42 +0900

    feat: impl tokenize

--- lib/sharpnesh/parser/lexer.rb
+++ lib/sharpnesh/parser/lexer.rb
@@ -33,8 +33,24 @@ class Sharpnesh::Parser
 
     private
 
-    def tokenize
+    RULES = [
+      [/^[a-zA-Z_][a-zA-Z_0-9]*/, :name]
+    ].freeze
 
+    def tokenize
+      RULES.each do |pattern, type|
+        pattern.match(source, pos) do |md|
+          body = md[0]
+          token = Token.new(type, body, @line, @col)
+          @col += body.size
+          @pos += body.size
+          @next += 1
+          @tokens << token
+          break token
+        end
+      end
+
+      raise ParseError, 'cannot recognize charactor'
     end
 
     Token = Struct.new('Token', :type, :body, :line, :col)
