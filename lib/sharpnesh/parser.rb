308999125853b8088193735bc4982b1ff3abe068
autopp <autopp.inc@gmail.com>
2018-05-08 09:52:10 +0900

    feat: impl parsing list

--- lib/sharpnesh/parser.rb
+++ lib/sharpnesh/parser.rb
@@ -3,6 +3,17 @@ module Sharpnesh
   #
   class Parser
     def parse(io, name)
+      lexer = Lexer.new(io, name)
+      parse_list(lexer)
+    end
+
+    def parse_list(lexer)
+      pipelines = parse_pipelines(lexer)
+      terminal = lexer.next
+      unless %i[newline eos semicolon &].include?(terminal.type)
+        raise ParseError, "unexpected token #{terminal.body}"
+      end
+      Node.new(:list, body: pipelines, terminal: terminal.body)
     end
 
     class ParseError < StandardError
