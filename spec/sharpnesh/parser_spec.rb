cf38548f262cfcace783fe69e037f6decefb9e01
autopp <autopp.inc@gmail.com>
2018-05-17 09:49:51 +0900

    test: add test case for two command

--- spec/sharpnesh/parser_spec.rb
+++ spec/sharpnesh/parser_spec.rb
@@ -33,5 +33,26 @@ describe Sharpnesh::Parser do
 
       it { is_expected.to eq(expected) }
     end
+
+    context 'with two commands' do
+      let(:src) { 'foo; bar' }
+      let(:expected) do
+        n(:list,
+          body: n(:pipeline,
+                  excl: nil,
+                  command: n(:simple_command,
+                             assigns: [], body: [n(:name, body: 'foo')])),
+          terminal: '', next: n(
+            :list,
+            body: n(:pipeline,
+                    excl: nil,
+                    command: n(:simple_command,
+                               assigns: [], body: [n(:name, body: 'bar')])),
+            terminal: '', next: nil
+          ))
+      end
+
+      it { is_expected.to eq(expected) }
+    end
   end
 end
