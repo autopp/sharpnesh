fe54ba8f4a4a63023f4832f183f4636a7189debd
autopp <autopp.inc@gmail.com>
2018-06-15 09:46:48 +0900

    test: add test case for array expansions

--- spec/sharpnesh/parser_spec.rb
+++ spec/sharpnesh/parser_spec.rb
@@ -218,6 +218,23 @@ describe Sharpnesh::Parser do
       it { is_expected.to eq(expected) }
     end
 
+    context 'with array expansions' do
+      let(:src) { '$foo[*] ${foo[@]}' }
+      let(:root_list) do
+        [
+          n(:pipelines,
+            body: n(:pipeline,
+                    excl: nil,
+                    command: n(:simple_command,
+                               assigns: [],
+                               body: [n(:array_ex, array: 'foo', mode: '*'), n(:array_ex, array: 'foo', mode: '@')])),
+            terminal: nil)
+        ]
+      end
+
+      it { pending; is_expected.to eq(expected) }
+    end
+
     context 'with a single quoted string' do
       let(:src) { "foo 'bar baz'" }
       let(:root_list) do
